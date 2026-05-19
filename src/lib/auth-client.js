8e5e72a18cbc85af7de7e9e053020ab19467205b
arafat33498-prog <arafat33598@gmail.com>
2026-05-19 23:16:42 +0600

    some issus

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,13 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  // 🎯 সরাসরি আপনার লাইভ রেন্ডার সার্ভারের লিংক
-  baseURL: "https://docappoint-server-ewq6.onrender.com", 
-  
-  fetchOptions: {
-    credentials: "include", // ক্রস-ডোমেন কুকির জন্য বাধ্যতামূলক
-    onError: async (context) => {
-      console.error("Auth Client Fetch Error:", context);
-    },
-  },
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
